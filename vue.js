90822c8e3a0836be05c3d5a5dbb7e908d3e781a2
hukancheng <285840242@qq.com>
2019-03-28 00:41:27 +0800

    注册自定义事件

--- vue.js
+++ vue.js
@@ -2,7 +2,7 @@ function Vue(op) {
   this.data = op.data,
   this.mothods = op.mothods
   let id = op.el
-  this._proptype_ = {}
+  this._ob_ = {}
   let dom = nodeToFragment(document.getElementById(id),this)
   // let dom = document.getElementById("appp").innerHTML = "Hello World";
   document.getElementById(id).appendChild(dom)
@@ -11,7 +11,7 @@ function Vue(op) {
 
 function nodeToFragment(node,vm) {
   // debugger
-  console.log(node.childNodes,9999)
+  // console.log(node.childNodes,9999)
   let newDocument = document.createDocumentFragment();
   let child;
   // let i = 0
@@ -26,19 +26,33 @@ function nodeToFragment(node,vm) {
   //   newDocument.appendChild(child)
   // }
   for (let i = 0; i < node.childNodes.length; i++) {
+    if(node.childNodes[i].nodeType == 1 && node.childNodes[i].attributes.length > 0) { 
+      console.log(node.childNodes[i].attributes, 'zidingyi')
+      initEvent(node,i,vm)
+    }
     if(node.childNodes[i].childNodes.length > 0){
       console.log(node.childNodes[i], '有子节点:',node.childNodes[i].childNodes.length)
-      nodeToFragment(node.childNodes[i])
-      // newDocument.appendChild(newNode,vm)
+      let newNode = nodeToFragment(node.childNodes[i])
+      newDocument.appendChild(newNode,vm)
     } else {
       comfile(node.childNodes[i],vm)
     }
-    newDocument.appendChild(node.childNodes[i])
-    console.log(newDocument,77777)
+    // newDocument.appendChild(node.childNodes[i])
+    // console.log(newDocument,77777)
   }
   return newDocument
 }
 
 function comfile (node,vm) {
-  console.log(node,333)
+  // console.log(node,333)
+}
+function initEvent (node,index,vm) {
+  let attr = node.childNodes[index].attributes
+  for (let i = 0;i < attr.length; i++) {
+    console.log(attr,99)
+    if (attr[i].nodeName == 'on-click') {
+      let functionName = attr[i].nodeValue;
+      node.addEventListener('click',() => {console.log('注册点击事件')})
+    }
+  }
 }
